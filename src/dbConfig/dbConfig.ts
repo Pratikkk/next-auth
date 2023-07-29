fb21f0c3e10f32075b185b0ffe2111571cf3173a
Pratikkk <kenipratik9@gmail.com>
2023-07-30 00:02:35 +0530

    some fixes

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,22 +1,21 @@
 import mongoose from "mongoose";
 
 export async function connect() {
-	try {
-		mongoose.connect(process.env.MONGO_URI!);
-		const connection = mongoose.connection;
+  try {
+    mongoose.connect(process.env.MONGO_URI!);
+    const connection = mongoose.connection;
 
-		connection.on("connected", () => {
-			console.log("Mongodb connected successfully");
-		});
-		connection.on("error", (err) => {
-			console.log(
-				"Mongodb connection error, Please  make sure Mongodb is running " +
-					err
-			);
-			process.exit();
-		});
-	} catch (error) {
-		console.log("Something goes wrong");
-		console.log(error);
-	}
+    connection.on("connected", () => {
+      console.log("Mongodb connected successfully");
+    });
+    connection.on("error", (err) => {
+      console.log(
+        "Mongodb connection error, Please  make sure Mongodb is running " + err
+      );
+      process.exit();
+    });
+  } catch (error) {
+    console.log("Something goes wrong");
+    console.log(error);
+  }
 }
